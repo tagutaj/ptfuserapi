5efc86c03b9f92029b0a0d10b4bfb8a97e689e20
tagutaj <tagutaj@gmail.com>
2022-03-05 20:05:25 +0200

    pagination, filtration, cache control

--- myusers/views.py
+++ myusers/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie, vary_on_headers
+
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated,IsAdminUser
+from rest_framework import filters
 
 from myusers.serializers import UserSerializer, SignupSerializer
 
-# Create your views here.
-
+# caching
+@method_decorator([vary_on_headers("Authorization",),vary_on_cookie, cache_page(60*60*2)], name='dispatch')
 class UserList(generics.ListAPIView):
+    
     permission_classes = [IsAuthenticated,IsAdminUser ]
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['first_name','last_name', 'email']
+    ordering_fields =  ['first_name','last_name', 'email']
 
 class SignupView(generics.CreateAPIView):
     permission_classes = [AllowAny]
