42434887f294977d6a48f1da75f28162b2cf4dbd
tagutaj <tagutaj@gmail.com>
2022-03-06 09:39:22 +0000

    deploy

--- userAPI/settings.py
+++ userAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1h08#jhmih3z8hrimpfvid)r-13_052783#qz81-spb!@&@y=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["codemajor.pythonanywhere.com",]
 
 #pagination
 
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static", ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
